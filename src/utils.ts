cba3e30ae972a95c2fe5a28450aea15985540fb3
voltria <trungnguyen.31241022633@st.ueh.edu.vn>
2026-05-23 22:41:43 +0700

    Add files via upload

---
+++ src/utils.ts
@@ -0,0 +1,17 @@
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function formatCurrency(amount: number) {
+  return new Intl.NumberFormat('vi-VN', {
+    style: 'currency',
+    currency: 'VND',
+  }).format(amount);
+}
+
+export function formatDate(dateString: string) {
+  return new Date(dateString).toLocaleString('vi-VN');
+}
